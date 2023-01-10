a17076bd1ff953653d2f7671d4e88284a825df23
daoninhthai <thaimeo1131@gmail.com>
2023-01-10 20:00:00 +0700

    init: Spring Boot 3.0 booking-api project

---
+++ src/main/java/com/daoninhthai/booking/BookingApiApplication.java
@@ -0,0 +1,12 @@
+package com.daoninhthai.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingApiApplication.class, args);
+    }
+}
