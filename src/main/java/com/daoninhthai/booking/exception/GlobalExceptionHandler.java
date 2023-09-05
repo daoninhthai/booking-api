99954a9fc7ef2d736592a7be9d43b106ed2fb039
daoninhthai <thaimeo1131@gmail.com>
2023-09-05 20:30:00 +0700

    feat: multi-language support (i18n)

--- src/main/java/com/daoninhthai/booking/exception/GlobalExceptionHandler.java
+++ src/main/java/com/daoninhthai/booking/exception/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.daoninhthai.booking.exception;
 
 import com.daoninhthai.booking.dto.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -9,10 +12,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
+
+    private final MessageSource messageSource;
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ApiResponse<Void>> handleResourceNotFound(ResourceNotFoundException ex) {
@@ -29,6 +36,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiResponse<Map<String, String>>> handleValidationExceptions(
             MethodArgumentNotValidException ex) {
+        Locale locale = LocaleContextHolder.getLocale();
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach(error -> {
             String fieldName = ((FieldError) error).getField();
@@ -36,9 +44,11 @@ public class GlobalExceptionHandler {
             errors.put(fieldName, errorMessage);
         });
 
+        String message = messageSource.getMessage("error.validation.failed", null, "Validation failed", locale);
+
         ApiResponse<Map<String, String>> response = ApiResponse.<Map<String, String>>builder()
                 .success(false)
-                .message("Validation failed")
+                .message(message)
                 .data(errors)
                 .build();
 
@@ -47,7 +57,9 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Void>> handleGenericException(Exception ex) {
+        Locale locale = LocaleContextHolder.getLocale();
+        String message = messageSource.getMessage("error.unexpected", null, "An unexpected error occurred", locale);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(ApiResponse.error("An unexpected error occurred: " + ex.getMessage()));
+                .body(ApiResponse.error(message + ": " + ex.getMessage()));
     }
 }
